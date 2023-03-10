547951320ff95373f92a9f1df9efc3ba2dbaae2c
tegumai <tgum@protonmail.com>
2023-03-10 22:06:21 +0100

    fixed up mobile stuff

--- sketch.js
+++ sketch.js
@@ -18,7 +18,8 @@ let data = {
 	x: 200,
 	y: 300,
 	color: [0, 255, 255],
-	name: "tegumai"
+	name: "tegumai",
+	last_active: 0
 }
 let user
 ref.on("value", (data) => {
@@ -42,7 +43,12 @@ function setup() {
 	}
 	user = ref.push(data).getKey()
 
+	joystick.r = height/5
+  joystick.pos = createVector(width - joystick.r/1.5, height - joystick.r/1.5)
+  joystick.pressed = false
 }
+
+let joystick = {}
 
 function draw() {
 	background(0)
@@ -73,6 +79,10 @@ function draw() {
 						userRef.remove()
 					}
 				}
+				if (game[k].last_active + 1000 < Date.now()) {
+					let kref = database.ref("main/" + k)
+					kref.remove()
+				}
 			}
 
 			try {
@@ -93,60 +103,32 @@ function draw() {
 		// 	noLoop()
 		// }
 		try {
-
+			let dir = [0,0]
 			if (keyIsDown(39)) {
-				let x = game[user].x + speed
-				if (x >= width) {
-					x = 0
-				}
-				let userRef = database.ref("main/" + user)
-				userRef.set({
-					name: game[user].name,
-					x: x,
-					y: game[user].y,
-					color: game[user].color
-				})
+				dir[0] += 1
 			}
 			if (keyIsDown(37)) {
-				let x = game[user].x - speed
-				if (x <= 0) {
-					x = width
-				}
-				let userRef = database.ref("main/" + user)
-				userRef.set({
-					name: game[user].name,
-					x: x,
-					y: game[user].y,
-					color: game[user].color
-				})
+				dir[0] -= 1
 			}
 			if (keyIsDown(40)) {
-				let y = game[user].y + speed
-				if (y >= height) {
-					y = 0
-				}
-				let userRef = database.ref("main/" + user)
-				userRef.set({
-					name: game[user].name,
-					x: game[user].x,
-					y: y,
-					color: game[user].color
-				})
+				dir[1] += 1
 			}
 			if (keyIsDown(38)) {
-				let y = game[user].y - speed
-				if (y <= 0) {
-					y = height
-				}
-				let userRef = database.ref("main/" + user)
-				userRef.set({
-					name: game[user].name,
-					x: game[user].x,
-					y: y,
-					color: game[user].color
-				})
+				dir[1] -=1
 			}
-		} catch (e) {}
+
+			joy_dir = handleJoystick()
+			if (dir[0]==0&&dir[1]==0) dir = joy_dir
+
+			let userRef = database.ref("main/" + user)
+			userRef.set({
+				name: game[user].name,
+				x: game[user].x + dir[0] * speed,
+				y: game[user].y + dir[1] * speed,
+				color: game[user].color,
+				last_active: Date.now()
+			})
+		} catch (e) {console.error(e)}
 	}
 }
 
@@ -161,3 +143,39 @@ window.onbeforeunload = () => {
 	let userRef = database.ref("main/" + user)
 	userRef.remove()
 }
+
+function handleJoystick() {
+	push()
+	strokeWeight(10)
+	let userRef = database.ref("main/" + user)
+	stroke(game[user].color)
+	noFill()
+	ellipse(joystick.pos.x, joystick.pos.y, joystick.r)
+
+	let mouse = createVector(mouseX, mouseY)
+	if (mouse.dist(joystick.pos) < joystick.r/2 && mouseIsPressed) {
+		joystick.pressed = true
+	}
+	if (!mouseIsPressed) {
+		joystick.pressed = false
+	}
+
+	let dir = [0, 0]
+	if (joystick.pressed) {
+		let rel = p5.Vector.sub(mouse, joystick.pos).limit(joystick.r/2)
+		let new_mouse = p5.Vector.add(joystick.pos, rel)
+		ellipse(new_mouse.x, new_mouse.y, joystick.r/3.5)
+		dir = [rel.x / joystick.r * 2, rel.y / joystick.r * 2]
+	}
+
+	pop()
+
+	return dir
+}
+
+function isMobile() {
+  let check = false;
+  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+  return check;
+};
+
